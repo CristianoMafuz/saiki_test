f663132f97235b5799ccf9069a70f0b6d3bb8631
CristianoMafuz <116692157+CristianoMafuz@users.noreply.github.com>
2025-07-12 18:45:38 -0300

    Update settings.py

--- saiki_django/settings.py
+++ saiki_django/settings.py
@@ -140,6 +140,8 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 #     path.join(BASE_DIR, "../frontend/"),
 # ]
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
